179e07ebfbd624727504e435a969f41bd8765fef
Dani Baas <Dani.raj25@gmail.com>
2021-03-18 15:34:17 +0100

    :see_no_evil: yes big progress

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { errorHandler } from "./middlewares/errorhandler";
+import weather from "./routes/weather";
 
 const app = express();
 app.use(express.json());
@@ -7,5 +9,14 @@ app.get("/", (_res, req) => {
     req.send("Hello World!");
 });
 
+app.use("/weather", weather);
+app.use(errorHandler);
+
 // Default port for dokku is 5000.
 app.listen(5000, () => console.log("Running on " + 5000));
+app.post("/okidoki", (req, res) => {
+    const text: string = req.body.text;
+    res.json({ success: true, text });
+});
+
+(async () => {})();
