5d45d78389eb751400449f88d6bd15e686df1e9f
Dani Baas <Dani.raj25@gmail.com>
2021-03-18 17:01:36 +0100

    :sparkles: Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { errorHandler } from "./middlewares/errorhandler";
 import weather from "./routes/weather";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (_res, req) => {
     req.send("Hello World!");
